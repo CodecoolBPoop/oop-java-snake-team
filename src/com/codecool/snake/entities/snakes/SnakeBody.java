74ff96e174549922b5f22e20a883b34c82a6f3a0
Unknown <tibormaron@gmail.com>
2018-07-11 11:12:29 +0200

    multiplayer in beta phase

--- src/com/codecool/snake/entities/snakes/SnakeBody.java
+++ src/com/codecool/snake/entities/snakes/SnakeBody.java
@@ -19,10 +19,12 @@ public class SnakeBody extends GameEntity implements Animatable {
     private Queue<Vec2d> history = new LinkedList<>();
     private static final int historySize = 3;
 
-    public SnakeBody(Pane pane, GameEntity parent) {
+    public SnakeBody(Pane pane, GameEntity parent, int player) {
         super(pane);
         this.parent = parent;
-        setImage(Globals.snakeBody);
+
+        if (player == 1) setImage(Globals.snakeBody);
+        else setImage(Globals.snakeBody2);
 
         // place it visually below the current tail
         List<Node> children = pane.getChildren();
